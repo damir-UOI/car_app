3395b158bf5e76aa8bb84f3af087a608332e8dc6
Damir <damir@illinois.edu>
2026-06-14 23:00:37 -0500

    Added forms and validation

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+
 
 import sqlite3
 import os
@@ -41,7 +44,7 @@ def drivers():
     )
 
     rows = cursor.fetchall()
-
+    
     conn.close()
 
     return render_template(
@@ -86,7 +89,7 @@ def runs():
 
         ON runs.course_id = courses.id
 
-        LIMIT 50
+        
 
         """
 
@@ -100,6 +103,192 @@ def runs():
         "runs.html",
         runs=rows
     )
+
+
+
+
+#add driver route
+@app.route(
+    "/add_driver",
+    methods=["GET", "POST"]
+)
+
+def add_driver():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+
+        license_type = request.form["license_type"]
+
+        if len(name.strip()) == 0:
+
+            return render_template(
+                "add_driver.html",
+                error="Driver name required."
+            )
+
+        conn = sqlite3.connect(DB_PATH)
+
+        cursor = conn.cursor()
+
+        cursor.execute(
+
+            """
+
+            SELECT *
+
+            FROM drivers
+
+            WHERE LOWER(name) = LOWER(?)
+
+            """,
+
+            (name,)
+
+        )
+
+        existing_driver = cursor.fetchone()
+
+        if existing_driver:
+
+            conn.close()
+
+            return render_template(
+                "add_driver.html",
+                error="Driver already exists."
+            )
+
+        cursor.execute(
+
+            """
+
+            INSERT INTO drivers
+
+            (
+
+                name,
+
+                license_type
+
+            )
+
+            VALUES (?,?)
+
+            """,
+
+            (
+
+                name,
+
+                license_type
+
+            )
+
+        )
+
+        conn.commit()
+
+        conn.close()
+
+        return redirect("/drivers")
+
+    return render_template(
+        "add_driver.html",
+        error=None
+    )
+
+
+
+
+#add run route
+@app.route(
+    "/add_run",
+    methods=["GET", "POST"]
+)
+
+def add_run():
+    if request.method == "POST":
+
+        try:
+            
+            driver_id = int(request.form["driver_id"])
+            course_id = int(request.form["course_id"])
+            run_time = float(request.form["run_time"])
+            cones = int(request.form["cones"])
+        except ValueError:
+            return "Invalid input. Please enter valid numbers."
+        if run_time <= 0:
+            return "Run time must be a non-negative number."
+        if cones < 0:
+            return "Number of cones must be a non-negative integer."
+
+
+
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+
+               
+        cursor.execute(
+
+            """
+
+            INSERT INTO runs
+
+            (
+
+                driver_id,
+
+                course_id,
+
+                run_time,
+
+                cones
+
+            )
+
+            VALUES (?,?,?,?)
+
+            """,
+
+            (
+
+                driver_id,
+
+                course_id,
+
+                run_time,
+
+                cones
+
+            )
+
+        )
+
+        conn.commit()
+        conn.close()
+        return redirect("/runs")
+
+        
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+      
+    cursor.execute(
+        "SELECT * FROM drivers"
+    )
+    drivers = cursor.fetchall()
+            
+    cursor.execute(
+        "SELECT * FROM courses"
+    )
+    courses = cursor.fetchall()
+    
+    return render_template(
+        "add_run.html",
+        drivers=drivers,
+        courses=courses
+    )
+
 
 
 if __name__ == "__main__":
